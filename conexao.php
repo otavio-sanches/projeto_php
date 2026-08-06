bf0d2aab6b5ab2cf562a9fbced65cd226e03d341
otavio-sanches <jusileufaleceu@gmail.com>
2026-08-06 11:47:29 -0300

    conexão com o banco

--- conexao.php
+++ conexao.php
@@ -6,11 +6,5 @@
     $senha = "";
     $banco = "sistema_produtos";
     $conexao = mysqli_connect($host, $usuario, $senha, $banco);
-
-    if ($conexao) {
-        echo "Conexão realizada com sucesso!";
-    } else {
-        echo "Erro ao conectar: " . mysqli_connect_error();
-    }
-
+   
 ?>
