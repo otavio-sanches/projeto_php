7c64fd10a5f58d75d9ca39f1f42414486a9ef5b1
Otavio <jusileufaleceu@gmail.com>
2026-08-27 10:59:56 -0300

    Add files via upload

---
+++ projeto_php/index.php
@@ -0,0 +1,6 @@
+<?php require __DIR__ . '/cabecalho.php'; ?>
+    <main>
+        <p>Bem-vindo(a) ao sistema!</p>
+        <p>Hoje é <?php echo date('d/m/Y'); ?></p>
+    </main>
+<?php require __DIR__ . '/rodape.php'; ?>
