5d4e371f946ad5dad489ac92c1d6bd095e8deab5
Otavio <jusileufaleceu@gmail.com>
2026-08-27 12:01:30 -0300

    Add files via upload

--- produtos/listar.php
+++ produtos/listar.php
@@ -1,13 +1,38 @@
 <?php
 require __DIR__ . '/verifica_login.php';
-require __DIR__ . '/../cabecalho.php';
+require __DIR__ . '/../conexao.php';
+$sql = "SELECT * FROM produtos";
+$resultado = mysqli_query($conexao, $sql);
 ?>
-
+<?php require __DIR__ . '/../cabecalho.php'; ?>
 <main>
-    <p>Bem-vindo(a), <?php echo $_SESSION['usuario_nome']; ?>!</p>
-    <!-- conteúdo da página -->
-</main>
+    <div class="page-header">
+        <h2>Produtos cadastrados</h2>
+        <a class="btn btn-success" href="cadastrar.php">Cadastrar novo produto</a>
+    </div>
 
-<?php
-require __DIR__ . '/../rodape.php';
-?>
+    <div class="table-responsive">
+        <table>
+            <tr>
+                <th>Produto</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Qtd.</th>
+                <th>Ações</th>
+            </tr>
+            <?php while ($produto = mysqli_fetch_assoc($resultado)) { ?>
+                <tr>
+                    <td><?php echo $produto['nome']; ?></td>
+                    <td><?php echo $produto['descricao']; ?></td>
+                    <td>R$ <?php echo $produto['preco']; ?></td>
+                    <td><?php echo $produto['quantidade']; ?></td>
+                    <td class="acoes">
+                        <a class="btn btn-primary" href="atualizar.php?id=<?php echo $produto['id']; ?>">Editar</a>
+                        <a class="btn btn-danger" href="excluir.php?id=<?php echo $produto['id']; ?>">Excluir</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+</main>
+<?php require __DIR__ . '/../rodape.php'; ?>
